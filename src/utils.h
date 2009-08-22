8e88da8ee10dc79079f81ada292fe6cd1daf48af
Géraud Le Falher <daureg@gmail.com>
2009-08-22 19:32:53 +0200

    Swap, Rad/Deg and Random functions

--- src/utils.h
+++ src/utils.h
@@ -1,10 +1,8 @@
 #ifndef __UTILS
 #define __UTILS
-
+#include <cmath>
 #include "conf.h"
-
 const float EPSILON = 0.0005f;
-
 inline int Clamp(int value,int min, int max) {
 	if (value > max)
 		return max;
@@ -20,15 +18,27 @@ inline float Clamp(float value,float min, float max) {
 	return value;
 }
 inline int Random(int min, int max) {
-	return int((rand()/RAND_MAX)*(max-min)+min);
+	return int((rand()/float(RAND_MAX-1))*(max-min)+min);
 }
 inline float Random(float min, float max) {
 	return (rand()/float(RAND_MAX-1))*(max-min)+min;
 }
+inline float RadToDeg(float a) { return 180.0f*a/M_PI; }
+inline float DegToRad(float a) { return a*M_PI/180.0f; }
 inline bool cmp(float a, float b) {
 	if (a > b-EPSILON && a < b+EPSILON)
 		return true;
 	else
 		return false;
+}
+inline void Swap(float *t,Uint8 r1,Uint8 c1,Uint8 r2,Uint8 c2) {
+	float tmp = t[r1*4+c1];
+	t[r1*4+c1] = t[r2*4+c2];
+	t[r2*4+c2] = tmp;
+}
+inline void Swap(Uint8 *t,Uint8 a,Uint8 b) {
+	Uint8 tmp = t[a];
+	t[a] = t[b];
+	t[b] = tmp;
 }
 #endif

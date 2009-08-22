9c09021989ee67c0929e587e6cb0d6ca7cc3de92
Géraud Le Falher <daureg@gmail.com>
2009-08-22 19:22:31 +0200

    triangle.cpp
    add Triangle class ; OpenGL rendered facet

---
+++ src/triangle.h
@@ -0,0 +1,24 @@
+#ifndef __TRIANGLE
+#define __TRIANGLE
+#include <GL/gl.h>
+#include "conf.h"
+class Triangle {
+	public:
+		Triangle();
+		Triangle(Uint16*,Uint16*,Uint16*,Uint16*);
+		virtual ~Triangle();
+		void SetPoint(Uint16*,Uint16*,Uint16*,Uint16*);
+		void Draw() const;
+
+	private:
+		void Swap(Uint8,Uint8);
+		void Sort();
+		void Line(Uint16,Uint16,Uint16,Uint16,Uint16*) const;
+
+		Uint16 m_vertex[6];
+		Uint16 *m_l1;
+		Uint16 *m_l2;
+		Uint16 *m_l3;
+		bool m_default;
+};
+#endif

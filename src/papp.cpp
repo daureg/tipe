eaf54ad0727977177a17a90d61aa2f2e8009189b
Géraud Le Falher <daureg@gmail.com>
2009-08-30 21:16:02 +0200

    Perlin sample app rescales the heightmap

--- src/papp.cpp
+++ src/papp.cpp
@@ -1,9 +1,28 @@
 #include "papp.h"
 Papp::Papp():m_png("perlin.png"),m_perlin(256,5,0.7f) {
+	float tmp=0.0f;
+	Uint8 expand=0;
+	Uint16 i,j;
+	for (i=0;i<5;i++)
+		tmp+=pow(0.7f,i);
+	expand=Uint8(255/tmp);
+	Uint16 hmin=255,hmax=0,height;
 	m_t = new Uint8[PICTURE_SIZE*PICTURE_SIZE];
-	for (int i=0;i<PICTURE_SIZE;i++)
-		for (int j=0;j<PICTURE_SIZE;j++)
-			m_t[256*i+j]=220*m_perlin.Noise(i,j);
+	for (i=0;i<PICTURE_SIZE;i++)
+		for (j=0;j<PICTURE_SIZE;j++) {
+			height=expand*m_perlin.Noise(i,j);
+			if (height<hmin)
+				hmin=height;
+			else if (height>hmax)
+				hmax=height;
+			m_t[PICTURE_SIZE*i+j]=height;
+		}
+	printf("%d - %d\n",hmin,hmax);
+	// Rescale to 0-255
+	tmp=(255/float(hmax-hmin));
+	for (i=0;i<PICTURE_SIZE;i++)
+		for (j=0;j<PICTURE_SIZE;j++)
+			m_t[PICTURE_SIZE*i+j]=Uint8((m_t[PICTURE_SIZE*i+j]-hmin)*tmp);
 }
 bool Papp::Run() {return m_png.Write(m_t);}
 Papp::~Papp() {delete [] m_t;}

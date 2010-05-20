c8bc91cd6b17827225e766122e89adb1ac4d297e
Géraud Le Falher <daureg@gmail.com>
2010-05-20 15:51:55 +0200

    draw only one triangle

--- src/triangle_app.cpp
+++ src/triangle_app.cpp
@@ -6,15 +6,18 @@ TriangleApp::TriangleApp() {
 	m_l1 = new Uint16[SCREEN_SIZE];
 	m_l2 = new Uint16[SCREEN_SIZE];
 	m_l3 = new Uint16[SCREEN_SIZE];
-	Uint16 t[6]={700,500,650,400,550,450};
+	Uint16 t[6]={765,550,650,350,535,550};
 	for (Uint8 j = 0; j < NBTR; j++) {
-		for (Uint8 i = 0; i < 3; i++) {
+	/*	for (Uint8 i = 0; i < 3; i++) {
 			t[2*i]=Random(10,RES_X-10);
 			t[2*i+1]=Random(10,RES_Y-10);
-		}
+		}*/
 		m_trs[j].SetPoint(t,m_l1,m_l2,m_l3);
 	}
-
+	Color c1=Red;
+	Color c2=Green;
+	Color c3=Blue;
+	m_trs[0].SetColors(&c1,&c2,&c3);
 }
 TriangleApp::~TriangleApp() {	
 	delete [] m_l1;
